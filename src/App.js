0dff28008d723aa4fc4dcebc055fe10bb056c02f
vbrmnd <vaibhavrmande@gmail.com>
2018-06-08 07:48:26 +0530

    Adding ant design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react'
-import TabBar from './TabBar'
+import {Menu, Icon} from 'antd'
+import 'antd/dist/antd.css';
 import './App.css';
 
 class App extends Component {
+  state = {
+    selected: 'log'
+  }
+
+  handleMenuClick = (event) => {
+    this.setState({selected: event.key})
+  }
+
   render() {
     return (
-      <TabBar selected={0}/>
+      <Menu
+        onClick={this.handleMenuClick}
+        selectedKeys={[this.state.selected]}
+        mode="horizontal"
+        theme="dark"
+      >
+        <Menu.Item key="log">
+          <Icon type="mail"/>LOG
+        </Menu.Item>
+        <Menu.Item key="">
+          <Icon type="mail"/>GRAPH
+        </Menu.Item>
+        <Menu.Item key="settings">
+          <Icon type="mail"/>SETTINGS
+        </Menu.Item>
+        <Menu.Item key="about">
+          <a href="https://www.github.com/vbrmnd" target="_blank" rel="noopener noreferrer">PROFILE</a>
+        </Menu.Item>
+      </Menu>
     );
   }
 }
