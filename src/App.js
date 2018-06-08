b81aafc8e26747ffb5d437b01a3bb6ed5bf2bc51
vbrmnd <vaibhavrmande@gmail.com>
2018-06-08 10:43:07 +0530

    adding header component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,20 @@
 import React, { Component } from 'react'
-import {Menu, Icon} from 'antd'
-import 'antd/dist/antd.css';
+import Header from './Header'
 import './App.css';
 
 class App extends Component {
   state = {
-    selected: 'log'
+    'dbHandle': null
   }
 
-  handleMenuClick = (event) => {
-    this.setState({selected: event.key})
+  componentDidMount() {
+    // check if dbHandle set, if not switch to settings and ask user to select the dbHandle
+    // search localStorage for dbHandle before switching to Settings menu
   }
 
   render() {
     return (
-      <Menu
-        onClick={this.handleMenuClick}
-        selectedKeys={[this.state.selected]}
-        mode="horizontal"
-        theme="dark"
-      >
-        <Menu.Item key="log">
-          <Icon type="mail"/>LOG
-        </Menu.Item>
-        <Menu.Item key="">
-          <Icon type="mail"/>GRAPH
-        </Menu.Item>
-        <Menu.Item key="settings">
-          <Icon type="mail"/>SETTINGS
-        </Menu.Item>
-        <Menu.Item key="about">
-          <a href="https://www.github.com/vbrmnd" target="_blank" rel="noopener noreferrer">PROFILE</a>
-        </Menu.Item>
-      </Menu>
+      <Header />
     );
   }
 }
---
+++ src/Header.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react'
+import {Menu, Icon} from 'antd'
+import 'antd/dist/antd.css';
+
+export default class Header extends Component {
+  state = {
+    selected: 'log'
+  }
+
+  handleMenuClick = (event) => {
+    this.setState({selected: event.key})
+  }
+
+  render() {
+    return (
+      <Menu
+        onClick={this.handleMenuClick}
+        selectedKeys={[this.state.selected]}
+        mode="horizontal"
+        theme="dark"
+      >
+        <Menu.Item key="log">
+          <Icon type="mail"/>LOG
+        </Menu.Item>
+        <Menu.Item key="">
+          <Icon type="mail"/>GRAPH
+        </Menu.Item>
+        <Menu.Item key="settings">
+          <Icon type="mail"/>SETTINGS
+        </Menu.Item>
+        <Menu.Item key="about">
+          <a href="https://www.github.com/vbrmnd" target="_blank" rel="noopener noreferrer">PROFILE</a>
+        </Menu.Item>
+      </Menu>
+    );
+  }
+}
