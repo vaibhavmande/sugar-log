4e755139c60e55f0e8650ac8fa78cb99d9055d1c
vaibhavmande <vaibhavrmande@gmail.com>
2019-03-22 17:57:13 +0530

    Added insert form

--- src/Header.js
+++ src/Header.js
@@ -7,6 +7,7 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import AddLogForm from './AddLogForm'
 
 const styles = {
   root: {
@@ -23,11 +24,20 @@ const styles = {
 
 class Header extends Component {
   state = {
-    selected: this.props.selected
+    selected: this.props.selected,
+    formActive: false
   }
 
   handleMenuClick = (event) => {
     this.setState({selected: event.key})
+  }
+
+  activateForm = () => {
+    this.setState({formActive: true})
+  }
+
+  deactivateForm = () => {
+    this.setState({formActive: false})
   }
 
   render() {
@@ -40,9 +50,14 @@ class Header extends Component {
               <MenuIcon />
             </IconButton>
             <Typography variant="h6" color="inherit" className={classes.grow}>
-              News
+              Home
             </Typography>
-            <Button color="inherit">Login</Button>
+            <Button variant="outlined" color="secondary" onClick={this.activateForm}>
+              Add
+              {this.state.formActive && 
+                <AddLogForm deactivateForm={this.deactivateForm}/>
+              }
+            </Button>
           </Toolbar>
         </AppBar>
       </div> 
