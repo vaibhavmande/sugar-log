7adc9c7e5307512da09a5aa2bd4febdfb781fbe7
Vaibhav Mande <vaibhavrmande@gmail.com>
2021-04-27 08:21:59 +0530

    extended flexcontainer

--- src/components/blocks/AppContainer/AppContainer.js
+++ src/components/blocks/AppContainer/AppContainer.js
@@ -3,20 +3,18 @@ import FlexContainer from '../../containers/FlexContainer'
 import Trend from '../Trend/Trend'
 import Readings from '../Readings/Readings'
 import Action from '../Action/Action'
+import styled from 'styled-components'
 
+const ColoredBackground = styled(FlexContainer)`
+  background-color: #eee;
+`
 const AppContainer = () => {
   return (
-    <FlexContainer
-      fd="column"
-      ai="center"
-      jc="center"
-      flex="1"
-      style={{ backgroundColor: '#eee' }}
-    >
+    <ColoredBackground fd="column" ai="center" jc="center" flex="1">
       <Trend></Trend>
       <Readings></Readings>
       <Action></Action>
-    </FlexContainer>
+    </ColoredBackground>
   )
 }
 
